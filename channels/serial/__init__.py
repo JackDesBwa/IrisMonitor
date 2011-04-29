6ebdd0b456ae63667ea76e9444a993dd40006450
JackDesBwa <jackdesbwa@gmail.com>
2011-04-29 08:40:52 +0200

    No serial channel if pyserial is not available

--- channels/serial/__init__.py
+++ channels/serial/__init__.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import serial as rs232
 import config, events
 from channels import IrisChannel
+try:
+	import serial as rs232
 
-class serial(IrisChannel):
-	def __init__(self):
-		# TODO : Configuration
-		self.serial = rs232.Serial('./virtual_tty', baudrate=38400, bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=0, rtscts=0)
+	class serial(IrisChannel):
+		def __init__(self):
+			# TODO : Configuration
+			self.serial = rs232.Serial('./virtual_tty', baudrate=38400, bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=0, rtscts=0)
+				
 
-	def run(self):
-		while 1:
-			events.fire(IrisChannel, 'onReceive', self.serial.read(1))
+		def run(self):
+			while 1:
+				events.fire(IrisChannel, 'onReceive', self.serial.read(1))
+except:
+	pass
