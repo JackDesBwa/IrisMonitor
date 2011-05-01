f218408893bc0e3a97e44bb77235ca2eacb88db4
JackDesBwa <jackdesbwa@gmail.com>
2011-05-02 01:06:19 +0200

    Inclusion of channels test into decoders test for less code copy

--- channels/test.py
+++ channels/test.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 import sys, os
-sys.path.remove(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+if os.path.dirname(os.path.abspath(__file__)) in sys.path:
+	sys.path.remove(os.path.dirname(os.path.abspath(__file__)))
+if os.path.dirname(os.path.dirname(os.path.abspath(__file__))) not in sys.path:
+	sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import channels, config, events
 from io import open
 
--- decoders/test.py
+++ decoders/test.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 import sys, os
-sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
-import channels, decoders, events
+if os.path.dirname(os.path.abspath(__file__)) in sys.path:
+	sys.path.remove(os.path.dirname(os.path.abspath(__file__)))
+if os.path.dirname(os.path.dirname(os.path.abspath(__file__))) not in sys.path:
+	sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import decoders, events
 
 def onAllMessages(event, data):
 	sys.stdout.write('%f : ' % data[0])
@@ -37,33 +40,5 @@ except KeyboardInterrupt:
 	sys.exit('Interrupted test.\n')
 
 sys.stdout.write('The decoder has to be tested in conjunction with a channel.\n')
-sys.stdout.write('Available channels are :\n')
-i = 0
-chanlist = channels.get_list()
-for c in chanlist:
-	sys.stdout.write('  %3d    %s\n' % (i, c))
-	i += 1
 
-try:
-	if len(chanlist) == 0:
-		sys.stderr.write('No channel available.\n')
-	if len(chanlist) == 1:
-		a = 0
-	else:
-		sys.stdout.write('Which one would be used ? ')
-		sys.stdout.flush()
-		a = int(sys.stdin.readline())
-	sys.stdout.write('Test with `%s`\n' % chanlist[a])
-	theclass = channels.get_class(chanlist[a])
-	chan = theclass()
-except ValueError:
-	sys.stderr.write('Invalid number.\n')
-except IndexError:
-	sys.stderr.write('Unknowed entry.\n')
-except KeyboardInterrupt:
-	sys.exit('Interrupted test.\n')
-
-try:
-	chan.run()
-except KeyboardInterrupt:
-	sys.stdout.write('End of test.\n')
+from channels import test
