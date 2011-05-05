348af9d962f50153442c6d4624d9ccd3aa0c19f1
JackDesBwa <jackdesbwa@gmail.com>
2011-05-05 18:34:38 +0200

    New parameter in UDP channel

--- channels/udp/__init__.py
+++ channels/udp/__init__.py
@@ -9,15 +9,19 @@ class udp(IrisChannel):
 	def __init__(self):
 		IrisChannel.__init__(self)
 		self.config = config.ConfigList(self.__class__, {
+			'host' : config.ConfigString('Accepted hosts mask', 'localhost'),
 			'port' : config.ConfigInt('Port to listen', 0, 65535, 50000)
 		})
 
 	def loop_init(self):
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		self.sock.bind(('localhost', self.config.port))
+		self.sock.bind((self.config.host, self.config.port))
 	
 	def loop(self):
 		msg, addr = self.sock.recvfrom(1024)
 		for byte in msg:
 			if not byte: byte = '\x00'
 			events.fire(IrisChannel, 'onReceive', byte)
+	
+	def loop_finish(self):
+		self.sock.close()
